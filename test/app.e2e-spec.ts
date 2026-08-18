a8cc51ad62b30f6b4639a79f5bc1d8b95173dde0
John Gomez <johngomezdev@gmail.com>
2026-08-18 16:43:40 -0500

    feat: configure enviroment for e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,11 +16,12 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(404);
+    
+      expect(response.status).toBe(404);
   });
 
   afterEach(async () => {
